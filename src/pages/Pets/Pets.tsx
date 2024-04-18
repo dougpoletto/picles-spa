e0343a87e7c27b59e6fd1e7ace8eb5f12262ee5e
dougpoletto <polettodoug@hotmail.com>
2024-04-17 22:24:56 -0300

    Criação de componentes modelos request e response e client de requisição

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css";
+import { Skeleton } from "../../components/common/Skeleton";
 
 export function Pets() {
+  // const [pets, setPets] = useState<IPet[]>([]);
+
+  // useEffect(() => {
+  //   getPets().then((data) => {
+  //     setPets(data.items);
+  //   })
+  // }, [])
+  
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
-        <Link to="/pets/20">Ir para listagem</Link>
+        <main className={styles.list}>
+          <Skeleton count={5} containerClassName={styles.skeleton}/>
+          {/* {
+            pets.map(pet => (
+              <Card key={pet.id} href={`/pets/${pet.id}`} text={pet.name} thumb={pet.photo} />    
+            ))
+          } */}
+        </main>
       </div>
     </Grid>
   )
