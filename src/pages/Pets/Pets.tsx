67c62316ada40d8d859e5dc5fa07fc36e96af9a5
dougpoletto <polettodoug@hotmail.com>
2024-04-18 22:27:56 -0300

    Criação da paginação e lista de pets

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,29 +1,45 @@
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css";
+import { Card } from "../../components/common/Card";
 import { Skeleton } from "../../components/common/Skeleton";
+import { Pagination } from "../../components/common/Pagination";
+import { useSearchParams } from "react-router-dom";
+import { usePetList } from "../../hooks/usetPetList";
 
 export function Pets() {
-  // const [pets, setPets] = useState<IPet[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlParams = { page: searchParams.get("page") ? Number(searchParams.get("page")) : 1 };
+  const { data, isLoading } = usePetList(urlParams);
 
-  // useEffect(() => {
-  //   getPets().then((data) => {
-  //     setPets(data.items);
-  //   })
-  // }, [])
-  
+  function changePage(page: number) {
+    setSearchParams((params) => {
+      params.set("page", String(page));
+      return params
+    })
+  }
+
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
+        {
+          isLoading && <Skeleton containerClassName={styles.sekeleton} count={data?.items.length} />
+        }
         <main className={styles.list}>
-          <Skeleton count={5} containerClassName={styles.skeleton}/>
-          {/* {
-            pets.map(pet => (
-              <Card key={pet.id} href={`/pets/${pet.id}`} text={pet.name} thumb={pet.photo} />    
+          {
+            data?.items.map(pet => (
+              <Card key={pet.id} href={`/pets/${pet.id}`} text={pet.name} thumb={pet.photo} />
             ))
-          } */}
+          }
         </main>
+        {data?.currentPage && (
+          <Pagination
+            currentPage={data.currentPage}
+            totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+          />
+        )}
       </div>
     </Grid>
   )
