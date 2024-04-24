3a5aec8d999fb457880d4adb70eb73954ee431c4
dougpoletto <polettodoug@hotmail.com>
2024-04-23 22:20:20 -0300

    Criação e implementação do componente de input

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,9 +1,18 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
+import styles from "./Shelter.module.css";
 
 export function Shelter() {
   return (
     <Panel>
-      Formulário do abrigo
+      <form className={styles.container}>
+        <Input label="Nome:" />
+        <Input label="Email:" />
+        <Input label="WhatsApp:" />
+        <Input label="Telefone:" />
+        <Button type="submit">Salvar Dados</Button>
+      </form>
     </Panel>
   )
 }
